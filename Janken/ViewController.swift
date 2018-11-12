ace610f45cc8d6fe99716fe853f501e8d03de7c2
y-nakane <y-nakane@sakura.ad.jp>
2018-11-12 13:28:30 +0900

    ボタンを押したらグーチョキパーを順番に出すようにコード変更

--- Janken/ViewController.swift
+++ Janken/ViewController.swift
@@ -18,9 +18,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerImageView: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
     
+    var answerNumber = 0
+    
     @IBAction func shuffleAction(_ sender: Any) {
-        answerLabel.text = "グー"
-        answerImageView.image = UIImage(named: "gu")
+        if answerNumber == 0 {
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named: "gu")
+        } else if answerNumber == 1 {
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named: "choki")
+        } else if answerNumber == 2 {
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named: "pa")
+        }
+        
+        answerNumber += 1
     }
 }
 
